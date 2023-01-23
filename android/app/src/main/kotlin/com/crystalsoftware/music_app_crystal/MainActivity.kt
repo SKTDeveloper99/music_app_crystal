ac80e17a7de476c58684051481333ea033330801
SKTran <66289215+SKTDeveloper99@users.noreply.github.com>
2023-01-23 15:44:42 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/crystalsoftware/music_app_crystal/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.crystalsoftware.music_app_crystal
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/base_navigation.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:music_app_crystal/example_playlist.dart';
+
+class BaseNavigation extends StatefulWidget {
+  const BaseNavigation({super.key});
+
+  @override
+  State<BaseNavigation> createState() => _BaseNavigationState();
+}
+
+class _BaseNavigationState extends State<BaseNavigation> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    PlaylistPage(),
+    Text(
+      'Index 2: Fun Interaction',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: Settings',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Crystal Musical App'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            backgroundColor: Colors.red,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.library_music),
+            label: 'Playlist',
+            backgroundColor: Colors.green,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.soup_kitchen_sharp),
+            label: 'Surprise',
+            backgroundColor: Colors.purple,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+            backgroundColor: Colors.pink,
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
